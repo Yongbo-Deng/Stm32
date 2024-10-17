7d5456d49bbb739415802a0b5d5b89e1e9ec9822
eBean <yongbo.dengyb@gmail.com>
2024-10-17 15:44:09 -0700

    OLED

--- 4.1OLED/User/main.c
+++ 4.1OLED/User/main.c
@@ -1,18 +1,20 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-#include "LED.h"
-#include "Key.h"
+#include "OLED.h"
 
 int main(void)
 {
-	uint8_t key;
-	LED_Init();
-	Key_Init();
+	OLED_Init();
+	
+	OLED_ShowChar(1, 1, 'A');
+	OLED_ShowString(1, 3, "HelloWorld!");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 2);
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);
+	
 	while (1)
 	{
-		key = Key_getNum();
-		if (key != 0) {
-			Turn(key);
-		}
+		
 	}
 }
