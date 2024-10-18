199998e131910288da9fb3b346dede2799ef9697
eBean <yongbo.dengyb@gmail.com>
2024-10-17 20:28:56 -0700

    CountSensor

---
+++ 5.1CountSensor/User/main.c
@@ -0,0 +1,15 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "CountSensor.h"
+
+int main(void)
+{
+	CountSensor_Init();
+	OLED_Init();
+	OLED_ShowString(1, 1, "Count:");
+	while (1)
+	{
+		OLED_ShowNum(1, 7, CountSensor_Get(), 5);
+	}
+}
