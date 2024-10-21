ecd9691efdb60c7e5e6bef0ad5628c5b9b1b36a4
eBean <yongbo.dengyb@gmail.com>
2024-10-20 23:07:21 -0700

    PWM_Motro

---
+++ 6.5PWM_Motor/User/main.c
@@ -0,0 +1,35 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Motor.h"
+#include "Key.h"
+
+int8_t Speed;
+uint8_t KeyNum;
+
+int main(void)
+{
+	OLED_Init();
+	Motor_Init();
+	Key_Init();
+	
+	OLED_ShowString(1, 1, "Speed:");
+	Motor_SetSpeed(Speed = 20);
+
+	while (1)
+	{
+		KeyNum = Key_getNum();
+		if (KeyNum == 1) {
+			Speed += 20;
+			if (Speed > 100) {
+				Speed = -100;
+			}
+		}
+		Motor_SetSpeed(Speed);
+		OLED_ShowNum(1, 7, Speed, 3);
+	}
+}
+
+
+
+
