8417444c96431ebca048c7c600b20db7a6d4e515
eBean <yongbo.dengyb@gmail.com>
2024-10-23 11:25:25 -0700

    ADC muti-channel

--- 7.2ADC_Muti-Channal/Hardware/AD.c
+++ 7.2ADC_Muti-Channal/Hardware/AD.c
@@ -9,17 +9,17 @@ void AD_Init(void) {
 
     GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);
+    // ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);
 
     ADC_InitTypeDef ADC_InitStruct;
     ADC_InitStruct.ADC_Mode = ADC_Mode_Independent;
     ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
     ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-    ADC_InitStruct.ADC_ContinuousConvMode = ENABLE;
+    ADC_InitStruct.ADC_ContinuousConvMode = DISABLE;
     ADC_InitStruct.ADC_ScanConvMode = DISABLE;
     ADC_InitStruct.ADC_NbrOfChannel = 1;
     ADC_Init(ADC1, &ADC_InitStruct);
@@ -31,11 +31,12 @@ void AD_Init(void) {
     ADC_StartCalibration(ADC1);
     while (ADC_GetCalibrationStatus(ADC1) == SET);
 
-    ADC_SoftwareStartConvCmd(ADC1, ENABLE);     //Continous mode.
+    // ADC_SoftwareStartConvCmd(ADC1, ENABLE);     //Continous mode.
 }
 
-uint16_t AD_GetValue(void) {
-    // ADC_SoftwareStartConvCmd(ADC1, ENABLE);     //Discontinous mode.
+uint16_t AD_GetValue(uint8_t ADC_Channel) {
+    ADC_RegularChannelConfig(ADC1, ADC_Channel, 1, ADC_SampleTime_55Cycles5);
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE);     //Discontinous mode.
     while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
     return ADC_GetConversionValue(ADC1);    //Auto clear FlagStatus
 }
--- 7.2ADC_Muti-Channal/User/main.c
+++ 7.2ADC_Muti-Channal/User/main.c
@@ -3,21 +3,28 @@
 #include "OLED.h"
 #include "AD.h"
 
-float Voltage;
-uint16_t ADValue;
+uint16_t AD0, AD1, AD2, AD3; 
 
 int main(void) {
 	OLED_Init();
 	AD_Init();
 	
-	OLED_ShowString(1, 1, "Vi:0.0");
-	OLED_ShowString(2, 1, "AD:");
+	OLED_ShowString(1, 1, "AD0:0000");
+	OLED_ShowString(2, 1, "AD1:0000");
+	OLED_ShowString(3, 1, "AD2:0000");
+	OLED_ShowString(4, 1, "AD3:0000");
 	while (1){
-		ADValue = AD_GetValue();
-		Voltage = (float) ADValue / 4095 * 3.3;
-		OLED_ShowNum(1, 4, Voltage, 1);								//Integer
-		OLED_ShowNum(1, 6, (uint16_t) (Voltage * 100) % 100, 2);	//Decimal
-		OLED_ShowNum(2, 4, ADValue, 4);
+		AD0 = AD_GetValue(ADC_Channel_0);
+		AD1 = AD_GetValue(ADC_Channel_1);
+		AD2 = AD_GetValue(ADC_Channel_2);
+		AD3 = AD_GetValue(ADC_Channel_3);
+		
+		OLED_ShowNum(1, 5, AD0, 4);
+		OLED_ShowNum(2, 5, AD1, 4);
+		OLED_ShowNum(3, 5, AD2, 4);
+		OLED_ShowNum(4, 5, AD3, 4);
+
+		Delay_ms(100);
 
 	}
 }
