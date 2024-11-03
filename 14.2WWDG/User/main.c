5233ef0eb2d1614faf7c5d97f556aa1dcf3617b5
eBean <yongbo.dengyb@gmail.com>
2024-11-02 18:26:33 -0700

    done with watch dog

--- 14.2WWDG/User/main.c
+++ 14.2WWDG/User/main.c
@@ -10,9 +10,9 @@ int main(void) {
 
 	OLED_ShowString(1, 1, "IWDG TEST");
 	
-	if(RCC_GetFlagStatus(RCC_FLAG_IWDGRST) == SET) {
+	if(RCC_GetFlagStatus(RCC_FLAG_WWDGRST) == SET) {
 		//Reset by WWDG.
-		OLED_ShowString(2, 1, "IWDG Rest");
+		OLED_ShowString(2, 1, "WWDG Rest");
 		Delay_ms(500);
 		OLED_ShowString(2, 1, "          ");
 		RCC_ClearFlag();
@@ -23,19 +23,22 @@ int main(void) {
 		OLED_ShowString(2, 1, "    ");
 	}
 
-	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
-	IWDG_SetPrescaler(IWDG_Prescaler_16);		//0.8ms
-	IWDG_SetReload(2500 -1);	//2500 * 0.8 = 1000ms
-	IWDG_ReloadCounter();	
-	IWDG_Enable();		//Auto read only.
-	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);
+
+	WWDG_SetPrescaler(WWDG_Prescaler_8);	//27.77us
+	WWDG_SetWindowValue(0x40 | 21);			//30ms
+	WWDG_Enable(0x40 | 54);					//54 * 27.77 * 4098 = 50ms 
+	Delay_ms(31);
+
+
 	while (1){
 		KeyNum = Key_GetNum();
 
-		IWDG_ReloadCounter();
+		WWDG_SetCounter(0x40 | 54);
 		OLED_ShowString(4, 1, "FEED");
-		Delay_ms(600);	//My chip boundary about 900ms.
+		Delay_ms(20);	//My chip boundary about 900ms.
 		OLED_ShowString(4, 1, "    ");
-		Delay_ms(200);
+		Delay_ms(20);
+		
 	}
 }
